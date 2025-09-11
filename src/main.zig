d9d457599c4c63db084dd8978e4ad42092a86a50
Maciej Malinowski <maciej.malinowski@mercedes-benz.com>
2025-09-11 20:25:12 +0200

    clean

--- src/main.zig
+++ src/main.zig
@@ -1,4 +1,7 @@
+const print = @import("std").debug.print;
 const rl = @import("raylib");
+const rg = @import("raygui");
+const x = @import("assets/assets.zig");
 
 pub fn main() anyerror!void {
     // Initialization
@@ -9,6 +12,7 @@ pub fn main() anyerror!void {
     rl.initWindow(screenWidth, screenHeight, "raylib-zig [core] example - basic window");
     defer rl.closeWindow(); // Close window and OpenGL context
 
+    const texture = try rl.loadTextureFromImage(try rl.loadImageFromMemory(".png", x.TileSet));
     rl.setTargetFPS(60); // Set our game to run at 60 frames-per-second
     //--------------------------------------------------------------------------------------
 
@@ -26,7 +30,10 @@ pub fn main() anyerror!void {
 
         rl.clearBackground(.white);
 
+        const rect: rl.Rectangle = .{ .width = 85.0, .height = 75.0, .x = 0.0, .y = 100.0 };
+        _ = rg.button(rect, "press me");
         rl.drawText("Congrats! You created your first window!", 0, 0, 20, .light_gray);
+        rl.drawTexture(texture, 200, 200, .white);
         //----------------------------------------------------------------------------------
     }
 }
